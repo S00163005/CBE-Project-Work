476a351543f6e117bfeb7aa79f6e86f1f904d925
declanmartin <declanmartin@DEVLAPTOPJFORDE>
2018-04-03 16:15:40 +0100

    CloseButton Done

--- testProject3/testProject4/MainWindow.xaml.cs
+++ testProject3/testProject4/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace testProject4
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
